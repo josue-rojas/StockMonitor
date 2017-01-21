cb04945552f3056fa2a40404ee3351014c0f02dd
“[josue <[josuerojas.rojas@gmail.com]>
2017-01-20 20:03:12 -0500

    added a bunch of words
    added a bunch of words in separate files that state wether they are
    positive or negative
    added an empty .py file to be worked on to automate used of the words

---
+++ words.py
@@ -0,0 +1,11 @@
+'''
+This will be use to automate adding, deleting, or modifying words or all words
+Will include methods to:
+- Individually add a word
+- add a bunch of words (from other txt file or just input)
+- Delete a word 
+- Delete a bunch of words
+- Edit a word
+- Edit a bunch of words
+- Add another column to all words (add more properties)
+'''
---
+++ words/README.md
@@ -0,0 +1,4 @@
+#Words
+
+from a-z this is a list of words and a value of -1 for negative and 1 for positve
+this list will be used for supervised machine learning
